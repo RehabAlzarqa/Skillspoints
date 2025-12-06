d0b7a880afe73dcce198d30d5e3b86adf4d4a26f
rehab alzarqa <essamrehab98@gmail.com>
2025-12-06 22:49:34 +0100

    creat test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    setupFiles: './setupTests.js',
+    globals: true,
+    setupFiles: './test/setupTests.ts', 
   },
 });
