75eb37e8a77282a237293905f5cc38819d448907
RehabAlzarqa <75159618+RehabAlzarqa@users.noreply.github.com>
2026-01-14 13:52:09 +0100

    Feature/login api (#10)
    * feat(api): add login endpoint

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,23 +1,37 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import prisma from "@/lib/prisma";
+
 import { date } from "yup";
-export async function POST(request:Request) {
+
+
+export async function POST(request:NextRequest) {
     const body = await request.json();
 // validation here 
-    if (!body.name || ! body.email || ! body.password || !body.confirmpassword) {
+    if (!body.name || !body.email || !body.password || !body.confirmpassword) {
         return NextResponse.json(
             {error:"all field are requiresd "},
             {status:400}
         );
     }
-return NextResponse.json(
-  {
-    message:"done",
-    date:{
-        nome: body.name,
-        email: body.email,
-        password: body.password,
-    },
-  },
-  {status:201}
-);
-}
+    if (body.password !== body.confirmpassword) {
+        return NextResponse.json(
+          { error: "Passwords do not match" },
+          { status: 400 }
+        );
+      }
+      const hashedPassword = await bcrypt.hash(body.password, 10);
+      await prisma.utilisateur.create({
+        data: {
+            name: body.nome,
+            email: body.email,
+            motDePasse: hashedPassword,
+          }
+          
+      });
+      return NextResponse.json(
+        { message: "User created successfully" },
+        { status: 201 }
+      );
+            
+    }
