e9c258676e814281aaf918c57350c38d7129c201
rehab alzarqa <essamrehab98@gmail.com>
2026-02-06 10:38:43 +0100

    Update prisma and signup/login changes

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,37 +1,70 @@
+import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-import prisma from "@/lib/prisma";
-
-import { date } from "yup";
 
 
-export async function POST(request:NextRequest) {
+// steps 
+
+
+
+//create the endpoint function
+export async function POST(request: NextRequest) {
+
+  try {
+    //collect user details
     const body = await request.json();
-// validation here 
-    if (!body.name || !body.email || !body.password || !body.confirmpassword) {
-        return NextResponse.json(
-            {error:"all field are requiresd "},
-            {status:400}
-        );
+    const name = body.name;
+    const email = body.email;
+    const password = body.password;
+
+    //collect user details
+
+    // valaidate fields
+    if (!name || !email || !password) {
+      return NextResponse.json({ error: true, msg: " all field required" }, { status: 400 })
     }
-    if (body.password !== body.confirmpassword) {
-        return NextResponse.json(
-          { error: "Passwords do not match" },
-          { status: 400 }
-        );
-      }
-      const hashedPassword = await bcrypt.hash(body.password, 10);
-      await prisma.utilisateur.create({
-        data: {
-            name: body.nome,
-            email: body.email,
-            motDePasse: hashedPassword,
-          }
-          
-      });
-      return NextResponse.json(
-        { message: "User created successfully" },
-        { status: 201 }
-      );
-            
+    
+    // valaidate fields
+
+    //validate user existence
+    const utilisateurExist = await prisma.utilisateur.findUnique({ where: { email } })
+        return NextResponse.json({ error: true, msg: " all field required" }, { status: 400 })
+
+    if (utilisateurExist) {
+
+      return NextResponse.json({ error: true, msg: "The user with this email already exist. User another gmail" }, { status: 400 })
     }
+
+    //validate user existence
+
+    // create the user 
+    const utilsateurCreated = await prisma.utilisateur.create({
+     data: { name: name as string,
+      email: email,
+      motDePasse: password,}
+    })
+
+    // create the user 
+
+    // confirm is the user is not not saved
+
+    if (!utilsateurCreated) {
+      return NextResponse.json({ error: true, msg: "Error in creating your account" }, { status: 400 })
+    }
+    // confirm is the user is not not saved
+
+    // return the user to the frontend 
+
+    return NextResponse.json(
+      { error: false, msg: "  Your account was successfully created " },
+      { status: 201 }
+
+    )
+  } catch (error) {
+    console.log({error})
+    return NextResponse.json({ error })
+  }
+  //create the endpoint function
+
+
+}
+// return the user to the frontend
